c137d09d9b6b522018ee66f14ee25d8859c6a3a4
Lasya-939 <lasyareddy272@gmail.com>
2025-04-30 19:34:45 +0530

    Updated footer section

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer style={{ backgroundColor: '#2C3E50', color: 'white', textAlign: 'center', padding: '1rem' }}>
+            <div>
+                <p>© {new Date().getFullYear()} Starry Night Store - Powered by Superheroes</p>
+                <p>Follow us on:
+                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer"> Facebook</a> |
+                    <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer"> Instagram</a> |
+                    <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"> Twitter</a>
+                </p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer; // Make sure this line exists!
